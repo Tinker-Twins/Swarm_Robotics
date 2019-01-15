c43c62211ee43863aec60390bfa349f9ac776440
Samak Twins <samaktwins@gmail.com>
2019-01-15 14:31:44 +0530

    Add files via upload

---
+++ Master_Robot.ino
@@ -0,0 +1,58 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial BTSerial(2,3); // RX,TX
+
+void setup() {
+pinMode(4, OUTPUT);    // RIGHT MOTOR 1
+pinMode(5, OUTPUT);    // RIGHT MOTOR 2
+pinMode(6, OUTPUT);    // LEFT MOTOR 1
+pinMode(7, OUTPUT);    // LEFT MOTOR 2 
+Serial.begin(9600);
+BTSerial.begin(38400);
+}
+
+void loop() {
+
+BTSerial.println("f");
+digitalWrite (4, HIGH);         // FORWARD
+digitalWrite (6, HIGH);
+digitalWrite (5, LOW);
+digitalWrite (7, LOW);
+delay (5000);
+
+
+BTSerial.println("b");
+digitalWrite (4, LOW);          // BACKWARD
+digitalWrite (6, LOW);
+digitalWrite (5, HIGH);
+digitalWrite (7, HIGH);
+delay (5000);
+
+BTSerial.println("l");
+digitalWrite (4, HIGH);         // RADIAL LEFT
+digitalWrite (6, LOW);
+digitalWrite (5, LOW);
+digitalWrite (7, LOW);
+delay (5000);
+
+BTSerial.println("r");
+digitalWrite (4, LOW);          // RADIAL RIGHT
+digitalWrite (6, HIGH);
+digitalWrite (5, LOW);
+digitalWrite (7, LOW);
+delay (5000);
+
+BTSerial.println("L");
+digitalWrite (4, HIGH);         // AXIAL LEFT
+digitalWrite (6, LOW);
+digitalWrite (5, LOW);
+digitalWrite (7, HIGH);
+delay (5000);
+
+BTSerial.println("R");
+digitalWrite (4, LOW);          // AXIAL RIGHT
+digitalWrite (6, HIGH);
+digitalWrite (5, HIGH);
+digitalWrite (7, LOW);
+delay (5000);
+}
---
+++ Slave_Robot.ino
@@ -0,0 +1,58 @@
+void setup() {
+pinMode(4,OUTPUT);    // RIGHT MOTOR 1
+pinMode(5,OUTPUT);    // RIGHT MOTOR 2
+pinMode(6,OUTPUT);    // LEFT MOTOR 1
+pinMode(7,OUTPUT);    // LEFT MOTOR 2
+pinMode(8,OUTPUT);    // +5V FOR MOTOR DRIVER
+digitalWrite(8,HIGH);
+Serial.begin(38400);
+}
+
+void loop() {
+if(Serial.available()>0);{                                  // CHECK AVAILABILITY OF SERIAL COMMUNICATIN AND AVOID NULL [0] VALUES IF ANY
+  
+char x=Serial.read();                                       // DEFINE SERIAL READ CHARACTER
+
+if(x=='f'){                  // FORWARD
+  digitalWrite(4,HIGH);
+  digitalWrite(6,HIGH);
+  digitalWrite(5,LOW);
+  digitalWrite(7,LOW);
+}
+
+if(x=='b'){                 // BACKWARD
+  digitalWrite(4,LOW);
+  digitalWrite(6,LOW);
+  digitalWrite(5,HIGH);
+  digitalWrite(7,HIGH);
+}
+
+if(x=='l'){                 // RADIAL LEFT
+  digitalWrite (4,HIGH);
+  digitalWrite (6,LOW);
+  digitalWrite (5,LOW);
+  digitalWrite (7,LOW);
+}
+
+if(x=='r'){                // RADIAL RIGHT
+  digitalWrite (4,LOW);
+  digitalWrite (6,HIGH);
+  digitalWrite (5,LOW);
+  digitalWrite (7,LOW);
+}
+
+if(x=='L'){                 // AXIAL LEFT
+  digitalWrite (4,HIGH);
+  digitalWrite (6,LOW);
+  digitalWrite (5,LOW);
+  digitalWrite (7,HIGH);
+}
+
+if(x=='R'){                 // AXIAL RIGHT
+  digitalWrite (4,LOW);
+  digitalWrite (6,HIGH);
+  digitalWrite (5,HIGH);
+  digitalWrite (7,LOW);
+}
+}
+}
